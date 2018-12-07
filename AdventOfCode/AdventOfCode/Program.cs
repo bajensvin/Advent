67e0210acbbf240d9f4eed190acabe2372994fae
Michael Lindfors <michael.lindfors@datscha.com>
2018-12-07 23:05:10 +0100

    Parts 1 + 2

--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -2,11 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AdventOfCode {
     class Program {
-        static void Main(string[] args) {
+        static void Main() {
+            Console.WriteLine("Part 1 value: " + Part1() + "");
+            Console.WriteLine("Part 2 value: " + Part2() + "");
+            Console.ReadKey();
+        }
+        public static string[] ReadFile() {
+            var input = System.IO.File.ReadAllLines(@"./input.txt");
+            return input;
+        }
+        public static int Part1() {
+            var input = ReadFile();
+            return input.Sum(int.Parse);
+        }
+
+        public static int Part2() {
+            var input = ReadFile().ToList();
+            int currentTotalFrequency = 0;
+            HashSet<int> totalFrequencyAfterEachLoop = new HashSet<int>();
+            bool duplicate = false;
+
+            while (!duplicate) {
+                foreach (var item in input) {
+                    currentTotalFrequency = currentTotalFrequency + int.Parse(item);
+                    duplicate = totalFrequencyAfterEachLoop.Contains(currentTotalFrequency);
+                    totalFrequencyAfterEachLoop.Add(currentTotalFrequency);
+                    if (duplicate) {
+                        break;
+                    }
+                }
+            }
+            return currentTotalFrequency;
         }
     }
 }
