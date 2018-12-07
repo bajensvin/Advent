1e64102173eac26958e9f0f7d4fe44e5c5b56a5d
Michael Lindfors <michael.lindfors@datscha.com>
2018-12-07 23:54:40 +0100

    -

--- AdventOfCode/AdventOfCode/Day1.cs
+++ AdventOfCode/AdventOfCode/Day1.cs
@@ -6,27 +6,25 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace AdventOfCode {
-    class Program {
+    class Day1 {
         static void Main() {
             Console.WriteLine("Part 1 value: " + Part1() + "");
             Console.WriteLine("Part 2 value: " + Part2() + "");
             Console.ReadKey();
         }
         public static string[] ReadFile() {
-            var input = System.IO.File.ReadAllLines(@"./input.txt");
+            var input = System.IO.File.ReadAllLines(@"./Input/Day_1/input.txt");
             return input;
         }
         public static int Part1() {
             var input = ReadFile();
             return input.Sum(int.Parse);
         }
-
         public static int Part2() {
             var input = ReadFile().ToList();
             int currentTotalFrequency = 0;
             HashSet<int> totalFrequencyAfterEachLoop = new HashSet<int>();
             bool duplicate = false;
-
             while (!duplicate) {
                 foreach (var item in input) {
                     currentTotalFrequency = currentTotalFrequency + int.Parse(item);
