4f1efb2a19f06a561157c8ce4ab7e09b454497ee
Michael Lindfors <Michael.Lindfors@datscha.com>
2018-12-21 16:32:09 +0100

    Day 3 start

---
+++ AdventOfCode/AdventOfCode/Day_3/Day3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode {
+    static class Day3 {
+        public static string Part1(string[] input) {
+            var claims = new List<Claim>();
+            foreach (var row in input.ToList()) {
+                Claim claim = new Claim();
+                var capture = Regex.Match(row, "(#[0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)").Groups;
+                claim.Id = int.Parse(capture[1].Value.Replace("#", ""));
+                claim.FromLeft = int.Parse(capture[2].Value);
+                claim.FromTop = int.Parse(capture[3].Value);
+                claim.Width = int.Parse(capture[4].Value);
+                claim.Height = int.Parse(capture[5].Value);
+                claims.Add(claim);
+            }
+            //Draw area of dots
+            
+            return "";
+        }
+        public class Claim {
+            public int Id;
+            public int FromLeft;
+            public int FromTop;
+            public int Width;
+            public int Height;
+        }
+    }
+}
--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -7,10 +7,11 @@ using System.Threading.Tasks;
 namespace AdventOfCode {
     static class Program {
         static void Main() {
-            Console.WriteLine("Day 1 Part 1 value: " + Day1.Part1(Input.Read(@"./Day_1/Input/Input.txt")));
-            Console.WriteLine("Day 1 Part 2 value: " + Day1.Part2(Input.Read(@"./Day_1/Input/Input.txt")));
+            Console.WriteLine("Day 1 Part 1 value: " + Day1.Part1(Input.Read(@"./Day_1/Input/input.txt")));
+            Console.WriteLine("Day 1 Part 2 value: " + Day1.Part2(Input.Read(@"./Day_1/Input/input.txt")));
             Console.WriteLine("Day 2 Part 1 value: " + Day2.Part1(Input.Read(@"./Day_2/Input/input.txt")));
             Console.WriteLine("Day 2 Part 2 value: " + Day2.Part2(Input.Read(@"./Day_2/Input/input.txt")));
+            Console.WriteLine("Day 3 Part 1 value: " + Day3.Part1(Input.Read(@"./Day_3/Input/input.txt")));
             Console.ReadKey(true);
         }
     }
