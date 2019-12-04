649e74803faf98ac812e0ab09cffd70bf7d350b2
Michael Lindfors <Michael.Lindfors@datscha.com>
2019-12-04 17:02:15 +0100

    2019 day 1 part 1

---
+++ AdventOfCode/AdventOfCode/2019/Day_1/Day_1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2019.Day_1
+{
+    class Day_1
+    {
+        private static List<int> _values;
+        public static int Part1(string[] input)
+        {
+            _values = new List<int>();
+
+            foreach (var line in input)
+            {
+                var value = int.Parse(line) / 3;
+                _values.Add(value);
+            }
+
+            return _values.Sum(y => y - 2);
+        }
+
+        public static int Part2(string[] input)
+        {
+
+        }
+    }
+}
--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AdventOfCode._2019.Day_1;
 
 namespace AdventOfCode {
     static class Program {
         static void Main() {
-            Console.WriteLine("Day 1 Part 1 value: " + Day1.Part1(Input.Read(@"./Day_1/Input/input.txt")));
-            Console.WriteLine("Day 1 Part 2 value: " + Day1.Part2(Input.Read(@"./Day_1/Input/input.txt")));
-            Console.WriteLine("Day 2 Part 1 value: " + Day2.Part1(Input.Read(@"./Day_2/Input/input.txt")));
-            Console.WriteLine("Day 2 Part 2 value: " + Day2.Part2(Input.Read(@"./Day_2/Input/input.txt")));
-            Console.WriteLine("Day 3 Part 1 value: " + Day3.Part1(Input.Read(@"./Day_3/Input/input.txt")));
+            Console.WriteLine("2018 Day 1 Part 1 value: " + Day1.Part1(Input.Read(@"./2018/Day_1/Input/input.txt")));
+            Console.WriteLine("2018 Day 1 Part 2 value: " + Day1.Part2(Input.Read(@"./2018/Day_1/Input/input.txt")));
+            Console.WriteLine("2018 Day 2 Part 1 value: " + Day2.Part1(Input.Read(@"./2018/Day_2/Input/input.txt")));
+            Console.WriteLine("2018 Day 2 Part 2 value: " + Day2.Part2(Input.Read(@"./2018/Day_2/Input/input.txt")));
+            Console.WriteLine("2018 Day 3 Part 1 value: " + Day3.Part1(Input.Read(@"./2018/Day_3/Input/input.txt")));
+            Console.WriteLine($"2019 Day 1 Part 1 value: {Day_1.Part1(Input.Read(@"./2019/Day_1/Input/input.txt"))}");
             Console.ReadKey(true);
         }
     }
