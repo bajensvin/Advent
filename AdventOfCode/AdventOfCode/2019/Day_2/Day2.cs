77d29cdf315a0b3b8f0ef7c2217e0cc28674929c
Michael Lindfors <Michael.Lindfors@datscha.com>
2019-12-10 13:44:43 +0100

    Minor adjustments

--- AdventOfCode/AdventOfCode/2019/Day_2/Day2.cs
+++ AdventOfCode/AdventOfCode/2019/Day_2/Day2.cs
@@ -1,16 +1,16 @@
 ﻿namespace AdventOfCode._2019.Day_2
 {
-    public class Day2
+    internal static class Day2
     {
-        public static int Part1Value { get; set; }
-        public static int Part2Value { get; set; }
-        public static string[] Input { get; set; }
+        private static int Part1Value { get; set; }
+        private static int Part2Value { get; set; }
+        private static string[] Input { get; set; }
 
         public static int Part1()
         {
             Input = AdventOfCode.Input.Read(@"./2019/Day_2/Input/input.txt");
-            var occ = new OpCodeCalculator(Input);
-            Part1Value = occ.Calculate(12, 2);
+            var ocp = new OpCodeProgram(Input);
+            Part1Value = ocp.Calculate(12, 2);
             return Part1Value;
         }
 
@@ -20,8 +20,8 @@
             for (var i = 0; i < 100; i++)
             for (var j = 0; j < 100; j++)
             {
-                var occ = new OpCodeCalculator(Input);
-                var value = occ.Calculate(i, j);
+                var ocp = new OpCodeProgram(Input);
+                var value = ocp.Calculate(i, j);
                 if (value == 19690720)
                 {
                     Part2Value = 100 * i + j;
