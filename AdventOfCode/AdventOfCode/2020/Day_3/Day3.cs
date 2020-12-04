642d62e874315a3b94c38526660ab81fc5b6a171
Michael Lindfors <Michael.Lindfors@datscha.com>
2020-12-04 21:11:00 +0100

    touch up!

--- AdventOfCode/AdventOfCode/2020/Day_3/Day3.cs
+++ AdventOfCode/AdventOfCode/2020/Day_3/Day3.cs
@@ -9,31 +9,26 @@ namespace AdventOfCode._2020.Day_3
         public static List<string> Input = AdventOfCode.Input.Read(@"./2020/Day_3/Input/input.txt").ToList();
         public static int Part1()
         {
-            return GetTrees(1, 3);
+            return GetNumberOfTrees(1, 3);
         }
 
         public static int Part2()
         {
-            var first = GetTrees(1, 1);
-            var second = GetTrees(1, 3);
-            var third = GetTrees(1, 5);
-            var fourth = GetTrees(1, 7);
-            var fifth = GetTrees(2, 1);
-            return first * second * third * fourth * fifth;
+            return GetNumberOfTrees(1, 1) * GetNumberOfTrees(1, 3) * GetNumberOfTrees(1, 5) * GetNumberOfTrees(1, 7) * GetNumberOfTrees(2, 1);
         }
 
-        private static int GetTrees(int down, int right)
+        private static int GetNumberOfTrees(int moveDown, int moveRight)
         {
             var trees = 0;
             var currentHorizontalPosition = 0;
             for (var currentVerticalPosition = 0; currentVerticalPosition < Input.Count - 1;)
             {
-                //Go down 1
-                currentVerticalPosition += down;
-                //Go right 3
-                currentHorizontalPosition += right;
+                //Go down x
+                currentVerticalPosition += moveDown;
+                //Go right x
+                currentHorizontalPosition += moveRight;
                 var line = Input[currentVerticalPosition];
-                if (currentHorizontalPosition + right > Input[currentVerticalPosition].Length)
+                if (currentHorizontalPosition + moveRight > Input[currentVerticalPosition].Length)
                 {
                     for (var i = Input[currentVerticalPosition].Length - 1;
                         i < currentHorizontalPosition;
